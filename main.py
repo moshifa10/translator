a02a5099785cb783b12d8f62d4930fbd0d6b86ac
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-02-02 22:31:03 +0200

    added the images I am going to use and the data I will be using

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 
-# I will creator A project using tkinter where I use pandas and it prompt and you have a chance to answer
 
+BACKGROUND_COLOR = "#B1DDC6"
+# I will creator A project using tkinter where I use pandas and it prompt and you have a chance to answer
